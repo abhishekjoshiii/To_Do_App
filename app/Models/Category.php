b01bc3a273f44a0ac5946dcad8143c44393d9796
Kshitij chavan <88999875+Kshitijchavann@users.noreply.github.com>
2024-02-26 20:29:28 +0530

    created taskcategory migration and create model of users and category

---
+++ app/Models/Category.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Category extends Model
+{
+    use HasFactory, SoftDeletes;
+    
+    protected $table ='categories';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'name',
+        'description',
+        'user_id',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsto(User::class);
+    }
+
+    public function taskcategory(): belongsTo
+    {
+        return $this->belongsto(Taskcategory::class);
+    }
+
+}
