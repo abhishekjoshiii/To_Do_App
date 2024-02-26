42b105dd45054a6b53de1a63d89313f8353d24a1
Abhishek Joshi <joshiabhishek2255@gmail.com>
2024-02-26 19:50:32 +0530

    created migration for user and category

--- database/migrations/2024_02_26_114416_create_categories_table.php
+++ database/migrations/2024_02_26_114416_create_categories_table.php
@@ -11,13 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email', 191)->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('description', 255)->nullable();
+            $table->foreignId('user_id')->constrained();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
     }
 };
